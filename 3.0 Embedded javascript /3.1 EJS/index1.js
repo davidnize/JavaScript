5d4b54dc2f2fbd695e0b6e572ce1709e37de6c4a
davidnize <nizeyimanadavid61@gmail.com>
2026-02-05 00:01:20 +0200

    first commit

---
+++ 3.0 Embedded javascript /3.1 EJS/index1.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const today = new Date();
+  //Test code
+  // weekend:
+  // new Date("June 24, 2023 11:13:00");
+  // weekday:
+  // new Date("June 20, 2023 11:13:00");
+  const day = today.getDay();
+
+  let type = "a weekday";
+  let adv = "it's time to work hard";
+
+  if (day === 0 || day === 6) {
+    type = "the weekend";
+    adv = "it's time to have some fun";
+  }
+
+  res.render("index1.ejs", {
+    dayType: type,
+    advice: adv,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
